956f061f3fde8786842f183db81fb8ee442b920c
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-10-09 02:33:48 +0200

    Recive command line data

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include "map.h"
 using namespace std;
 
-int main(){
-	Map map (11,10);
-	map.generate();
-	map.print();
+int main(int argc, char const * argv[]){
+    int heigth;
+    int width;
+
+    if (argc < 3) {
+        heigth = 10;
+        width  = 24;
+    } else {
+        heigth = atoi(argv[1]);
+        width  = atoi(argv[2]);
+    }
+    Map map(heigth, width);
+
+    map.generate();
+    map.print();
+    return 0;
 }
