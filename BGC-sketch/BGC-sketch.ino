7d9263eac91b7f9a9d71d25e73d1e7d84be10698
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-01-21 16:59:28 +0100

    Arduino sketch

---
+++ BGC-sketch/BGC-sketch.ino
@@ -0,0 +1,253 @@
+#include "DHT.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include <SoftwareSerial.h>
+
+// Joystick
+#define HORZ 0 //Analog
+#define VERT 1 //Analog
+#define SEL 2 //Digital
+
+// DHT
+#define DHTTYPE DHT11
+#define DHTPIN 3 //Digital
+
+// Ultrasound
+#define TRIG 4 //Digital
+#define ECHO 5 //Digital
+
+// Heart Rate
+#define PULSE  2 //Analog -> Pulse Sensor purple wire
+#define BLINK  13 //Digital -> Pin to blink led at each beat
+
+typedef enum Direction {
+  UP = 0,
+  RIGHT = 90,
+  DOWN = 180,
+  LEFT = 270,
+  NONE = 360
+} Direction;
+
+typedef enum Camera {
+  CAMERA_UP = 0,
+  CAMERA_RIGHT = 1,
+  CAMERA_DOWN = 2,
+  CAMERA_LEFT = 3,
+  CAMERA_NONE = 4
+} Camera;
+
+typedef enum Shoot {
+  NO_SHOOT = 0,
+  SHOOT = 1
+} Shoot;
+
+typedef enum PauseResume {
+  PAUSE = 0,
+  RESUME = 1
+} PauseResume;
+
+typedef enum Velocity {
+  INCREASE = 0,
+  REDUCE = 1,
+  KEEP = 2
+} Velocity;
+
+typedef enum Texture {
+  WATER = 0,
+  LAVA = 1
+} Texture;
+
+int valX = 0;
+int valY = 0;
+int select = HIGH;
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+DHT dhtSensor(DHTPIN, DHTTYPE);
+int temperature;
+
+long distance = 0;
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+sensors_event_t event;
+
+// these variables are volatile because they are used during the interrupt service routine!
+volatile int bpm;                   // used to hold the pulse rate
+volatile int raw;                // holds the incoming raw data
+volatile int ibi = 600;             // holds the time between beats, the Inter-Beat Interval
+volatile boolean pulse = false;     // true when pulse wave is high, false when it's low
+volatile boolean qs = false;        // becomes true when Arduoino finds a beat.
+
+Camera cameraDirection;
+Shoot shoot;
+Texture texture;
+PauseResume pauseResume;
+Direction move;
+Velocity velocity;
+int n;
+int showBPM;
+int pauseGame;
+
+void setup() {
+  //Serial.begin(9600);
+  Serial.begin(115200);             // we agree to talk fast!
+
+  pinMode(SEL, INPUT);
+  digitalWrite(SEL, HIGH);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  if (!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while (1);
+  }
+  accel.setRange(ADXL345_RANGE_16_G);
+
+  pinMode(BLINK, OUTPUT);        // pin that will blink to your heartbeat!
+
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+
+  cameraDirection = CAMERA_NONE;
+  shoot = NO_SHOOT;
+  texture = WATER;
+  pauseResume = RESUME;
+  move = NONE;
+  velocity = KEEP;
+  n = 0;
+  showBPM = 1;
+  pauseGame = 0;
+  mySerial.begin(9600);
+}
+
+void loop() {
+  joystick();
+  dht();
+  ultrasound();
+  adxl();
+  heartRate();
+  if (mySerial.available()) {
+    char inChar = (char)mySerial.read(); //read the input
+    int inInt =  (char)mySerial.read() - '0';
+    Serial.println(inInt);
+    if (inChar == 'H') {
+      showBPM = inInt;
+    } else if (inChar == 'P') {
+      pauseGame = inInt;
+    }
+  }
+  delay(80);
+}
+
+void serialEvent() {
+  if (Serial.available()) {
+    Serial.read();
+    Serial.print("{");
+    // joystick
+    Serial.print("\"C\":"); Serial.print(cameraDirection); Serial.print(',');
+    Serial.print("\"S\":"); Serial.print(shoot);  Serial.print(',');
+    // dht
+    Serial.print("\"T\":"); Serial.print(texture); Serial.print(',');
+    // ultrasound
+    Serial.print("\"P\":"); Serial.print(pauseResume); Serial.print(',');
+    // adxl
+    Serial.print("\"M\":"); Serial.print(move); Serial.print(',');
+    // heartRate
+    Serial.print("\"V\":"); Serial.print(velocity);
+
+    Serial.print("}");
+
+    n++;
+    if (n >= 15) {
+      n = 0;
+      // joystick
+      mySerial.print("Joystick X: "); mySerial.println(valX);
+      mySerial.print("Joystick Y: "); mySerial.println(valY);
+      mySerial.print("Joystick Click: "); mySerial.println(shoot);
+      // dht
+      mySerial.print("Temperature: "); mySerial.println(temperature);
+      // ultrasound
+      mySerial.print("Distance: "); mySerial.println(distance);
+      // adxl
+      mySerial.print("ADXL X: "); mySerial.println(event.acceleration.x);
+      mySerial.print("ADXL Y: "); mySerial.println(event.acceleration.y);
+      // heartRate
+      mySerial.print("BPM: "); mySerial.print(bpm);
+    }
+  }
+}
+
+void joystick() {
+  valX = map(analogRead(HORZ), 0, 1023, 0, 19);
+  valY = map(analogRead(VERT), 0, 1023, 0, 19);
+  select = digitalRead(SEL);
+
+  bool horz = valX > 5 && valX < 15;
+  bool vert = valY > 5 && valY < 15;
+
+  if (vert && horz) cameraDirection = CAMERA_NONE;
+  else if (vert) cameraDirection = (valX >= 10) ? CAMERA_LEFT : CAMERA_RIGHT;
+  else cameraDirection = (valY >= 10) ? CAMERA_UP : CAMERA_DOWN;
+
+
+  shoot = (select != 1) ? shoot = SHOOT : NO_SHOOT;
+}
+
+void dht() {
+  temperature = getTemperature();
+  // std::cout << sensor.temperature << endl;
+  if (temperature != 0) {
+    texture = (temperature > 26) ? LAVA : WATER;
+  }
+}
+
+int getTemperature() {
+  return dhtSensor.readTemperature();
+}
+
+void ultrasound() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(TRIG, LOW);
+  long duration = pulseIn(ECHO, HIGH);
+  distance = microsecondsToCentimeters(duration);
+
+  pauseResume = (distance > 50 && distance < 800 || pauseGame == 1) ? PAUSE : RESUME;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+void adxl() {
+  accel.getEvent(&event);
+
+  bool horz = event.acceleration.x > -5 && event.acceleration.x < 5;
+  bool vert = event.acceleration.y > -5 && event.acceleration.y < 5;
+
+  if (vert && horz) move = NONE;
+  else if (vert) move = (event.acceleration.x >= 0) ? LEFT : RIGHT;
+  else move = (event.acceleration.y >= 0) ? UP : DOWN;
+}
+
+void heartRate() {
+
+  if (qs && showBPM == 1) {
+    if (bpm > 80 && bpm < 110) velocity = INCREASE;  // Reduce velocity
+    else if (bpm < 70) velocity = REDUCE;            // Increase velocity
+    else velocity = KEEP;
+  } else {
+    velocity = KEEP;
+  }
+}
+
---
+++ BGC-sketch/Interrupt.ino
@@ -0,0 +1,104 @@
+
+
+
+volatile int rate[10];                    // used to hold last ten IBI values
+volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
+volatile unsigned long lastBeatTime = 0;           // used to find the inter beat interval
+volatile int P =512;                      // used to find peak in pulse wave
+volatile int T = 512;                     // used to find trough in pulse wave
+volatile int thresh = 512;                // used to find instant moment of heart beat
+volatile int amp = 100;                   // used to hold amplitude of pulse waveform
+volatile boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat = true;       // used to seed rate array so we startup with reasonable BPM
+
+
+void interruptSetup(){     
+  // Initializes Timer2 to throw an interrupt every 2mS.
+  TCCR2A = 0x02;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
+  TCCR2B = 0x06;     // DON'T FORCE COMPARE, 256 PRESCALER 
+  OCR2A = 0X7C;      // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
+  TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
+  sei();             // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED      
+} 
+
+
+// THIS IS THE TIMER 2 INTERRUPT SERVICE ROUTINE. 
+// Timer 2 makes sure that we take a reading every 2 miliseconds
+ISR(TIMER2_COMPA_vect){                         // triggered when Timer2 counts to 124
+    cli();                                      // disable interrupts while we do this
+    raw = analogRead(PULSE);              // read the Pulse Sensor 
+    sampleCounter += 2;                         // keep track of the time in mS with this variable
+    int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+
+//  find the peak and trough of the pulse wave
+    if(raw < thresh && N > (ibi/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
+        if (raw < T){                        // T is the trough
+            T = raw;                         // keep track of lowest point in pulse wave 
+         }
+       }
+      
+    if(raw > thresh && raw > P){          // thresh condition helps avoid noise
+        P = raw;                             // P is the peak
+       }                                        // keep track of highest point in pulse wave
+    
+  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+  // signal surges up in value every time there is a pulse
+if (N > 250){                                   // avoid high frequency noise
+  if ( (raw > thresh) && (pulse == false) && (N > (ibi/5)*3) ){        
+    pulse = true;                               // set the Pulse flag when we think there is a pulse
+    digitalWrite(BLINK,HIGH);                // turn on pin 13 LED
+    ibi = sampleCounter - lastBeatTime;         // measure time between beats in mS
+    lastBeatTime = sampleCounter;               // keep track of time for next pulse
+         
+         if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
+             firstBeat = false;                 // clear firstBeat flag
+             return;                            // IBI value is unreliable so discard it
+            }   
+         if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
+            secondBeat = false;                 // clear secondBeat flag
+               for(int i=0; i<=9; i++){         // seed the running total to get a realisitic BPM at startup
+                    rate[i] = ibi;                      
+                    }
+            }
+          
+    // keep a running total of the last 10 IBI values
+    word runningTotal = 0;                   // clear the runningTotal variable    
+
+    for(int i=0; i<=8; i++){                // shift data in the rate array
+          rate[i] = rate[i+1];              // and drop the oldest IBI value 
+          runningTotal += rate[i];          // add up the 9 oldest IBI values
+        }
+        
+    rate[9] = ibi;                          // add the latest IBI to the rate array
+    runningTotal += rate[9];                // add the latest IBI to runningTotal
+    runningTotal /= 10;                     // average the last 10 IBI values 
+    bpm = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+    qs = true;                              // set Quantified Self flag 
+    // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+    }                       
+}
+
+  if (raw < thresh && pulse == true){     // when the values are going down, the beat is over
+      digitalWrite(BLINK,LOW);            // turn off pin 13 LED
+      pulse = false;                         // reset the Pulse flag so we can do it again
+      amp = P - T;                           // get amplitude of the pulse wave
+      thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+      P = thresh;                            // reset these for next time
+      T = thresh;
+     }
+  
+  if (N > 2500){                             // if 2.5 seconds go by without a beat
+      thresh = 512;                          // set thresh default
+      P = 512;                               // set P default
+      T = 512;                               // set T default
+      lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+      firstBeat = true;                      // set these to avoid noise
+      secondBeat = true;                     // when we get the heartbeat back
+     }
+  
+  sei();                                     // enable interrupts when youre done!
+}// end isr
+
+
+
+
