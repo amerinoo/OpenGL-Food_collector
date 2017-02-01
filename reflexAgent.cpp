8798770d80bb6b86bedd16c35449706a801c39e3
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-02-01 12:15:00 +0100

    Improvet expectimax to eat all the map food

--- reflexAgent.cpp
+++ reflexAgent.cpp
@@ -17,7 +17,7 @@ Direction ReflexAgent::getAction(){
     for (unsigned int i = 0; i < legalActions.size(); i++) {
         scores.push_back(evaluationFunction(*gameState, legalActions[i]));
     }
-    float bestScore = -99999999999999999;
+    float bestScore = INT_MIN;
     for (unsigned int i = 0; i < scores.size(); i++) {
         if (scores[i] > bestScore) {
             bestScore = scores[i];
@@ -38,10 +38,9 @@ float ReflexAgent::evaluationFunction(Map currentGameState, Direction direction)
     Cell * currentPosition = currentGameState.getPosition(agent1);
     Cell * nextPosition    = currentGameState.getNextState(currentPosition, direction);
 
-    vector<Cell *> food = gameState->getFood();
-    for (unsigned int i = 0; i < food.size(); i++) {
-        int d = getDistance(currentGameState, nextPosition, food[i]);
-        totalScore += (d == 0.0) ? 50 : 1.0 / (d * d);
-    }
+
+    int d = closestFoodDistance(currentGameState, nextPosition);
+
+    totalScore += (d == 0.0) ? 50 : 1.0 / (d * d);
     return totalScore;
 } // evaluationFunction
